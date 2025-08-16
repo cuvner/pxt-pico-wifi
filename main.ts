955d70764b043f9a505c554cef653c1a69fc914a
cuvner <chriscourage699@hotmail.com>
2025-08-16 13:28:58 +0100

    Update main.ts fixerr v0.3 revert back

--- main.ts
+++ main.ts
@@ -1,16 +1,62 @@
-/**
- * Adafruit IO Extension (test minimal)
- */
-//% color=#00AACC icon="\uf1eb"
-namespace aio {
+//% color=#0f9d58 icon="\uf1eb" block="Pico Modem"
+namespace picoModem {
+    function at(line: string) {
+        serial.writeLine(line)
+        basic.pause(50)
+    }
 
-    /**
-     * Init modem on TX RX
-     */
-    //% block="init modem TX %tx RX %rx"
-    //% tx.defl=SerialPin.P0 rx.defl=SerialPin.P1
-    export function init(tx: SerialPin, rx: SerialPin): void {
-        serial.redirect(tx, rx, BaudRate.Baud9600)
+    //% block="init modem TX %tx RX %rx at %baud"
+    //% tx.defl=SerialPin.P0 rx.defl=SerialPin.P1 baud.defl=BaudRate.BaudRate115200
+    export function init(tx: SerialPin, rx: SerialPin, baud: BaudRate) {
+        serial.redirect(tx, rx, baud)
         basic.pause(100)
+        at("AT?")
+    }
+
+    //% block="set Wi-Fi SSID %ssid password %password"
+    export function wifi(ssid: string, password: string) {
+        at("AT+WIFI=\"" + ssid + "\",\"" + password + "\"")
+    }
+
+    //% block="use Adafruit IO user %user key %key"
+    export function aio(user: string, key: string) {
+        at("AT+AIO=\"" + user + "\",\"" + key + "\"")
+    }
+
+    let _feeds: string = ""
+
+    //% block="clear feed list"
+    export function clearFeeds() {
+        _feeds = ""
+    }
+
+    //% block="add feed %name"
+    export function addFeed(name: string) {
+        if (_feeds.length > 0) {
+            _feeds = _feeds + "," + "\"" + name + "\""
+        } else {
+            _feeds = "\"" + name + "\""
+        }
+    }
+
+    //% block="send feed list to modem"
+    export function sendFeeds() {
+        if (_feeds.length > 0) {
+            at("AT+FEEDS=" + _feeds)
+        }
+    }
+
+    //% block="connect modem"
+    export function connect() {
+        at("AT+CONNECT")
+    }
+
+    //% block="send up to 3 numbers %v1 %v2 %v3"
+    //% v2.defl=0 v3.defl=0
+    export function sendNumbers(v1: number, v2?: number, v3?: number) {
+        let msg = "" + v1
+        if (v2 !== undefined) msg = msg + "," + v2
+        if (v3 !== undefined) msg = msg + "," + v3
+        serial.writeLine(msg)
     }
 }
